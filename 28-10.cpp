e9b0e9b3912a27c0a3b270e470db427a0d9209fe
xyith <jonasz.stysiak@gmail.com>
2025-10-28 09:09:04 +0100

    Update 28-10.cpp

--- 28-10.cpp
+++ 28-10.cpp
@@ -56,4 +56,52 @@ int main() {
    return 0;
 }
 
+zad 2
 
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+const int N = 10;
+int tab[N];
+
+
+int NSPNM(){
+   int maks_dl = 1, akt_dl = 1, maks_pocz = 0, akt_pocz = 0;
+   for(int i = 1; i < N; i++){
+       if(tab[i] >= tab[i-1]){
+           akt_dl++;
+           if(akt_dl > maks_dl){
+               maks_dl = akt_dl;
+               maks_pocz = akt_pocz;
+           }
+       }
+       else {
+           akt_dl = 1; akt_pocz = i;
+       }
+   }
+   return maks_pocz;
+}
+
+
+int main() {
+
+
+   for(int i = 0; i < N; i++){
+       cin>>tab[i];
+   }
+
+
+   int g = NSPNM();
+
+
+   do{
+       cout<<tab[g]<<" "; g++;
+   }
+   while(g < N && tab[g] >= tab[g-1]);
+  
+   return 0;
+}
+
+zad 3
