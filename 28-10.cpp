e1f8e56951c3e7f67a42609059cdb94309772511
xyith <jonasz.stysiak@gmail.com>
2025-10-28 09:02:18 +0100

    Add array analysis program in 28-10.cpp :sob:
    Implement a program to analyze an array of random integers.

---
+++ 28-10.cpp
@@ -0,0 +1,59 @@
+zad 1
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+const int N = 10;
+int A[N];
+
+void losuj(int A[]){
+   for(int i=0;i<N;i++)
+       A[i]=rand()%100-50;
+   A[rand()%N]=rand()%50;
+}
+
+void Wpisz(int A[]){
+   for(int i=0;i<N;i++){
+       cout<<A[i]<<" "<<endl;
+   }
+}
+
+int NSPNM(){
+   int maks_dl = 1, akt_dl = 1, maks_pocz = 0, akt_pocz = 0;
+   for(int i = 1; i < N; i++){
+       if(A[i] < A[i-1]){
+           akt_dl++;
+           if(akt_dl > maks_dl){
+               maks_dl = akt_dl;
+               maks_pocz = akt_pocz;
+           }
+       }
+       else {
+           akt_dl = 1; akt_pocz = i;
+       }
+   }
+   return maks_pocz;
+}
+
+
+int main() {
+
+    losuj(A);
+    Wpisz(A);
+
+
+    int g = NSPNM();
+
+
+   do{
+       cout<<A[g]<<" "; g++;
+   }
+   while(g < N && A[g] < A[g-1]);
+
+  
+   return 0;
+}
+
+
