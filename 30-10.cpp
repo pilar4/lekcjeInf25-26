d27fa394732b9c92e0578a41a77f150409500637
xyith <jonasz.stysiak@gmail.com>
2025-10-30 13:21:32 +0100

    Implement random number generation and leader function
    Added functions to generate random numbers and find the leader.

--- 30-10.cpp
+++ 30-10.cpp
@@ -1,4 +1,6 @@
-cw3 
+cw3
+
+
 #include <iostream>
 #include <cmath>
 #include <ctime>
@@ -41,6 +43,53 @@ int main() {
     test();
 
     cout<<endl<<lider();
+    
+    return 0;
+}
+
+
+cw 4
+
+#include <iostream>
+#include <cmath>
+#include <ctime>
+using namespace std;
+
+const int N = 10;
+int A[N];
+
+void los(){
+    int x=rand()%100;
+    for(int i=0; i<N; i++){
+        if(rand()%2) A[i]=rand()%100;
+        else A[i]=x;
+    }
+}
+
+void test(){
+    for(int i=0; i<N; i++){
+        cout<<A[i]<<" ";
+    }
+}
+
+int lider(){
+    int I=0, Ka;
+    sort(A, A + N);
+    Ka = A[N/2];
+    for(int i=0; i<N; i++){
+        if(A[i] == Ka) I++;
+    }
+    if(I>N/2) return Ka;
+    return -1;
+}
+
+int main() {
+
+    srand(time(NULL));
+    los();
+    test();
+
+    cout<<endl<<lider()<<endl;
     
     return 0;
 }
