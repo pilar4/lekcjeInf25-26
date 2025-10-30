6b889d620beb0c55140a7e67e7a3f40b18d1f7da
xyith <jonasz.stysiak@gmail.com>
2025-10-30 13:46:59 +0100

    Update !utility.cpp

--- !utility.cpp
+++ !utility.cpp
@@ -1,7 +1,28 @@
+void los(){
+    int idol;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            A[i][j] = rand()%2;
+        }
+    }
+    if(rand()%2){
+        idol = rand()%N;
+        for (int i=0; i<N; i++){
+            A[i][idol] = true;
+        }
+        for (int j=0; j<N; j++){
+            A[idol][j] = false;
+        }
+    }
+
+}
+
 void test(){
     for(int i=0; i<N; i++){
-        for(int j=0;j<N;j++){
+        for(int j=0; j<N; j++){
             if(i!=j) cout<<A[i][j]<<" ";
+            else cout<<"   ";
         }
+        cout<<endl;
     }
 }
