107965df2d6906273429b4f8bfe0b915b4c81970
pilar4 <112971080+pilar4@users.noreply.github.com>
2025-10-30 13:27:10 +0100

    Implement leader search and random element generation
    Added functions to find a leader in an array and generate random elements.

--- 30-10.cpp
+++ 30-10.cpp
@@ -1,5 +1,10 @@
-cw3
+cw. 1
+mozemy zapamietac ilosc sprawdzantch elementow w zmiennej pomocniczej i gdy przekroczy polowe rozmiaru tablicy to wiadomo ze nie bedzie lidera. 
 
+cw. 2
+Zrobilismy to na lekcji. O(n^2) i O(n)
+
+cw3
 
 #include <iostream>
 #include <cmath>
@@ -90,6 +95,73 @@ int main() {
     test();
 
     cout<<endl<<lider()<<endl;
+    
+    return 0;
+}
+
+cw 5
+#include <iostream>
+#include <cmath>
+#include <ctime>
+using namespace std;
+
+const int N = 10;
+int A[N];
+
+void los(){
+    int x=rand()%100;
+    for(int i=0; i<N; i++){
+        if(rand()%2) A[i]=rand()%100;
+        else A[i]=x;
+    }
+}
+
+void test(){
+    for(int i=0; i<N; i++){
+        cout<<A[i]<<" ";
+    }
+}
+
+int lider(){
+    int I;
+    for(int i=0; i<N; i++){
+        I = 0;
+        for(int j=0; j<N; j++){
+            if(A[j] == A[i]){
+                I++;
+            }
+        }
+        if(I>N/2) return A[i];
+    }
+    return -1;
+}
+
+int SzukajLidera(){
+    int kandydat, ile=0;
+    for(int i=0;i<N;i++){
+        if (!ile){
+            ile = 1;
+            kandydat = A[i];
+        } else if (A[i]==kandydat) ile++;
+        else ile--;
+    }
+    if(!ile) return-1;
+    ile=0;
+    for(int i=0;i<N;i++){
+        if(A[i]==kandydat) ile++;
+    }
+    if(ile>N/2) return kandydat;
+    else return -1;
+        
+}
+
+int main() {
+
+    srand(time(NULL));
+    los();
+    test();
+
+    cout<<endl<<SzukajLidera();
     
     return 0;
 }
