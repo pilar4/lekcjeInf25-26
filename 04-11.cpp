fe3f9f39cb125c03eae1c172ad0d0b6f8d5acfb1
pilar4 <112971080+pilar4@users.noreply.github.com>
2025-11-04 08:25:26 +0100

    Implement random array generation and sorting
    Added functions to generate random values, display the array, and sort columns.

--- 04-11.cpp
+++ 04-11.cpp
@@ -42,3 +42,61 @@ int main() {
     
     return 0;
 }
+
+
+
+
+zad 3
+#include <iostream>
+#include <cmath>
+#include <ctime>
+using namespace std;
+
+const int N = 5;
+int A[N][N];
+
+void los(){
+    int SzukajIdola;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            A[i][j] = rand()%10;
+        }
+    }
+}
+
+void test(){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            cout<<A[i][j]<<" ";
+
+        }
+        cout<<endl;
+    }
+}
+
+void sortujKolumny(int k){
+    int tab[N];
+    for(int i=0;i<N;i++){
+        tab[i] = A[i][k];
+    }
+    sort(tab, tab + N);
+    for(int i=0;i<N;i++){
+        A[i][k] = tab[i];
+    }
+}
+
+int main() {
+
+    srand(time(NULL));
+    los();
+    test();
+    for(int i=0;i<N;i++){
+        sortujKolumny(i);
+    }
+    cout<<endl<<endl;
+    test();
+    
+    
+    return 0;
+}
+
