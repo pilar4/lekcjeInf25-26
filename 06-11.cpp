137518195b7836f69a022ad93b9dcd155a4ea6af
pilar4 <112971080+pilar4@users.noreply.github.com>
2025-11-06 14:23:25 +0100

    Update 06-11.cpp

--- 06-11.cpp
+++ 06-11.cpp
@@ -1,5 +1,27 @@
 Ćw. 1
-a)a(n) = a(n-1) + 3 rekurencyjnie
- a(n) = 3n - 1
+a(n) = a(n-1) + 3 rekurencyjnie
+a(n) = 3n - 1
 
-b) n(i) = n(i-1)*3 + 1
+n(i) = n(i-1)*3 + 1      nastepna liczba to 200
+Ćw. 2
+w arkuszu (użycie funkcji SILNIA())
+Ćw. 3
+
+Ćw. 4
+
+Ćw. 5
+6 mnożeń
+Ćw. 6
+ten sam ciąg tylko przesunięty o jeden
+
+w arkuszu (w każdej następnej kratce np K5 wpisywać =K4+K3)
+Ćw. 7
+
+Ćw. 8
+
+Ćw. 9
+
+Ćw. 10
+
+
+
