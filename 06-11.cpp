6f5714d7f93c086b044253a4b37bbc8b972bf58e
karoluxxx <karol.firnis@gmail.com>
2025-11-06 14:24:17 +0100

    Update 06-11.cpp

--- 06-11.cpp
+++ 06-11.cpp
@@ -6,9 +6,52 @@ n(i) = n(i-1)*3 + 1      nastepna liczba to 200
 Ćw. 2
 w arkuszu (użycie funkcji SILNIA())
 Ćw. 3
+#include <iostream>
+
+
+using namespace std;
+
+
+
+
+long long sil(int n){
+   if(n<2){
+       return 1;
+   }
+   return sil(n-1)*n;
+}
+int main(){
+   int n;cin>>n;
+
+
+   cout<<endl<<sil(n);
+
+
+   return 0;
+}
 
 Ćw. 4
+#include <iostream>
 
+using namespace std;
+
+void DeNBin(int n){
+    if(n<10){
+
+        cout<<n<<endl;
+    }
+    else{
+        DeNBin(n/10);
+        cout<<n%10<<endl;
+    }
+}
+int main(){
+    int n;cin>>n;
+
+    DeNBin(n);
+
+    return 0;
+}
 Ćw. 5
 6 mnożeń
 Ćw. 6
