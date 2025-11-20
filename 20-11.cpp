6af1e3ec58440e80a0feb92e9e972f53140c51b3
karoluxxx <karol.firnis@gmail.com>
2025-11-20 13:16:52 +0100

    Update 20-11.cpp

--- 20-11.cpp
+++ 20-11.cpp
@@ -1,4 +1,34 @@
+cw 2
+a)
+#include <iostream>
 
+using namespace std;
+
+const int N=15;
+const int NOMIN[]={50000,20000,10000,5000,2000,1000,500,200,100,50,20,10,5,2,1};
+
+void Dajkase(int kwota, int Reszta[]){
+    for(int i=0;i<N;i++){
+        Reszta[i]=kwota/NOMIN[i];
+        kwota=kwota%NOMIN[i];
+    }
+}
+
+int main(){
+    int kwota, Reszta[N];
+    cin>>kwota;
+
+    Dajkase(kwota, Reszta);
+
+    for(int i=0;i<N;i++){
+        if(Reszta[i]>0){
+            cout<<NOMIN[i]<<" - "<<Reszta[i]<<endl;
+        }
+    }
+
+    return 0;
+}
+b)
 cw 3
 zrobilismy na lekcji, system:
 1 grosz
