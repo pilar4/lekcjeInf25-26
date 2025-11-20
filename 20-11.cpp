e50e360b3667662c60f69b1d29031a78a5bc7823
pilar4 <112971080+pilar4@users.noreply.github.com>
2025-11-20 13:43:10 +0100

    Implement array operations and random generation
    Added functions to generate, display, and calculate the maximum sum of a 2D array.

--- 20-11.cpp
+++ 20-11.cpp
@@ -1,7 +1,7 @@
 cw 1
 
 https://docs.google.com/spreadsheets/d/15P3SY1jOkwHXhrhsQHvGrNaqSVuZ1CKTTTpe-UQup84/edit?usp=sharing
-link do gugla
+link do google
 
 cw 2
 a)
@@ -84,3 +84,56 @@ zrobilismy na lekcji, system:
 100 zlotych
 300 zlotych
 400 zlotych
+
+cw5
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+const int N = 3;
+int A[N][N+1];
+
+void Losuj (int A[] [N + 1]){
+    for (int i = 0; i < N ;i++)
+        for (int j = 0; j < N ;j++)
+            A[i][j]=1+rand()%9;
+    for (int i = 0; i<N;i++){
+        A[N][i] = 0; 
+        A[i][N] = 0;
+    }
+}
+
+void Wypisz(int A[] [N + 1] ){
+    for (int i = 0; i<N;i++){
+        for (int j = 0; j < N ;j++) cout<<A[i][j]<<" ";
+            cout<<endl;
+    }
+}
+
+int MaksSuma(int A[] [N + 1] ){
+    int suma=A[0][0];
+    int w = 0, k = 0;
+        for (int i = 1; i < 2*N - 1 ;i++)
+        if (A[w + 1][k] > A[w][k + 1])
+        {
+            suma+=A[w+1] [k]; w++;
+        }
+        else
+        {
+            suma+=A[w] [k+1]; k++;
+        }
+    return suma;
+}
+
+int main(){
+    srand(time(NULL));
+    Wypisz(A);
+    cout<<'\n';
+    Losuj(A);
+    cout<<'\n';
+    cout<<MaksSuma(A);
+
+}
