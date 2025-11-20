d8f9324a1776b188a1f15e2748ce4f019ebb4623
pilar4 <112971080+pilar4@users.noreply.github.com>
2025-11-20 13:14:44 +0100

    Update 20-11.cpp

--- 20-11.cpp
+++ 20-11.cpp
@@ -1,1 +1,18 @@
 
+cw 3
+zrobilismy na lekcji, system:
+1 grosz
+3 grosze
+4 grosze
+10 groszy
+30 groszy
+40 groszy
+1 zlotowka
+3 zlotowki
+4 zlowowki
+10 zlotych
+30 zlotych
+40 zlotych
+100 zlotych
+300 zlotych
+400 zlotych
