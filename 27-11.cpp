de49574f9e8d5443312ee6a07d4ff8f63e130b0f
xyith <jonasz.stysiak@gmail.com>
2025-11-27 13:19:24 +0100

    Update 27-11.cpp

--- 27-11.cpp
+++ 27-11.cpp
@@ -4,3 +4,41 @@ metoda zachlanna - 8,5,1,1 nie jest optymalna
 cw 2 
 a.  na lekcji
 b. NIE
+
+cw 3
+
+#include <iostream>
+
+using namespace std;
+
+const int N = 3;
+const int NOM[] = {1, 5, 7};
+const int KWOTA = 10;
+
+int IleMonet(){
+
+    int ileM[KWOTA+1];
+    ileM[0] = 0;
+    for(int i=1; i<=KWOTA; i++){
+        ileM[i]=KWOTA+1;
+    }
+    for(int i=1; i<=KWOTA; i++){
+        for(int j=0; j<N; j++){
+            if(NOM[j] <= i){
+                if(ileM[i-NOM[j]]+1 < ileM[i]){
+                    ileM[i] = ileM[i-NOM[j]]+1;
+                }
+            }
+        }
+    }
+    return ileM[KWOTA];
+}
+
+int main(){
+
+    cout<<KWOTA<<" --> "<<IleMonet()<<endl;
+
+    return 0;
+}
+
+
