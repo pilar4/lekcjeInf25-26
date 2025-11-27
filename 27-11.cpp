fbcf9f48ea34cf4dac57be56a81cc1399ebe8a75
xyith <jonasz.stysiak@gmail.com>
2025-11-27 14:10:10 +0100

    Update 27-11.cpp

--- 27-11.cpp
+++ 27-11.cpp
@@ -96,3 +96,51 @@ int main(){
     return 0;
 }
 
+cwicz 8
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const int N = 5;
+const int makswaga = 17;
+
+struct przedmiot{
+    int wart, waga;
+};
+
+
+int backpack(przedmiot P[]){
+
+    int w[makswaga+1];
+    for(int i=0; i <= makswaga; i++){
+        w[i]=0;
+    }
+    for(int i=0; i <= makswaga; i++){
+        for(int j=0; j < N; j++){
+            if(P[j].waga <= i){
+                if(w[i-P[j].waga] + P[j].wart > w[i]){
+                    w[i] = w[i-P[j].waga] + P[j].wart;
+                }
+            }
+        }
+    }
+    return w[makswaga];
+}
+
+int main(){
+    
+    przedmiot P[N];
+    ifstream we("do-plecaka.txt");
+
+
+    for(int i=0; i<N; i++){
+        we>>P[i].wart>>P[i].waga;
+    }
+    we.close();
+
+    cout<<backpack(P)<<endl;
+
+    return 0;
+}
