f7d96f68e8d52c303dea5b5c6144f5cde82c6126
karoluxxx <karol.firnis@gmail.com>
2025-11-27 13:43:24 +0100

    Update 27-11.cpp

--- 27-11.cpp
+++ 27-11.cpp
@@ -41,4 +41,58 @@ int main(){
     return 0;
 }
 
+cw 4
+#include <iostream>
+
+using namespace std;
+
+const int N=4;
+const int KWOTA=78;
+const int NOMIN[]={1,2,4,5};
+
+int IleMon(int Reszta[]){
+
+    int IleM[KWOTA+1];
+    int ResztaM[KWOTA+1][N];
+    IleM[0]=0;
+
+    for(int i=0;i<N;i++){
+        ResztaM[0][i]=0;
+    }
+    for(int i=1;i<=KWOTA;i++){
+        IleM[i]=KWOTA+1;
+    }
+    for(int i=1;i<=KWOTA;i++){
+        for(int j=0;j<N;j++){
+            if(NOMIN[j]<=i){
+                if(IleM[i-NOMIN[j]]+1<IleM[i]){
+                    IleM[i]=IleM[i-NOMIN[j]]+1;
+                    for(int k=0;k<N;k++){
+                        ResztaM[i][k]=ResztaM[i-NOMIN[j]][k];
+                    }
+                    ResztaM[i][j]++;
+                }
+            }
+        }
+    }
+    for(int i=0;i<N;i++){
+        Reszta[i]=ResztaM[KWOTA][i];
+    }
+    return IleM[KWOTA];
+}
+
+int main(){
+    int Reszta[N];
+    for(int i=0;i<N;i++){
+        Reszta[i]=0;
+    }
+
+    IleMon(Reszta);
+
+    for(int i=0;i<N;i++){
+        cout<<"Nominal "<<NOMIN[i]<<" - "<<Reszta[i]<<endl;  
+    }
+
+    return 0;
+}
 
