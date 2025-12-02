c07a28638c2a41ad929be95a6abe249c2374a56c
karoluxxx <karol.firnis@gmail.com>
2025-12-02 09:07:16 +0100

    Update 2-12.cpp

--- 2-12.cpp
+++ 2-12.cpp
@@ -1,4 +1,56 @@
-nigga
+zad 2
+#include <iostream>
+
+using namespace std;
+
+const int N=4;
+int P[N][N];
+
+void los(){
+  for(int i=0; i<N; i++){
+      for(int j=0; j<N; j++){
+          P[i][j] = rand()%10;
+      }
+  }
+}
+
+void wypisz(){
+  for(int i=0; i<N; i++){
+      for(int j=0; j<N; j++){
+          cout<<P[i][j]<<" ";
+      }
+      cout<<endl;
+  }
+}
+
+int MaxSum(){
+   for(int i=1;i<N;i++){
+       P[0][i]+=P[0][i-1];
+       P[i][0]+=P[i-1][0];
+   }
+   for(int i=1;i<N;i++){
+       for(int j=1;j<N;j++){
+           P[i][j]+=max(P[i-1][j],P[i][j-1]);
+           cout<<" "<<max(P[i-1][j],P[i][j-1]);
+       }
+   }
+   return P[N-1][N-1];
+}
+
+
+int main(){
+   los();
+   wypisz();
+
+   cout<<endl;
+
+   int max=MaxSum();
+
+   cout<<endl<<" Maxsumto "<<max;
+  
+   return 0;
+}
+
 
 
 zad 3
