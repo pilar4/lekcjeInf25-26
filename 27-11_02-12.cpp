54f2149667a9fefc8666f3fbf16a8315fee97f48
pilar4 <112971080+pilar4@users.noreply.github.com>
2025-12-02 08:18:05 +0100

    Update 27-11_02-12.cpp

--- 27-11_02-12.cpp
+++ 27-11_02-12.cpp
@@ -207,5 +207,6 @@ int main(){
 }
 
 Ćwiczenie 11
-Autostrada i strategia: STRA
-Literatura i matematyka: TE lub AT
+ Autostrada i strategia: S(5) - T(6) - R(7) - A(8) - A(10)
+Literatura i matematyka: T(3) - E(4) - A(6) - T(7) - A(10)
+
