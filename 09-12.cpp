55a5d413cddb0aa06feb29f59117ece3983b1fff
pilar4 <112971080+pilar4@users.noreply.github.com>
2025-12-09 08:46:39 +0100

    Update 09-12.cpp

--- 09-12.cpp
+++ 09-12.cpp
@@ -186,6 +186,86 @@ Zad. 4
 Zad. 5
 
 Zad. 6
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+const int N = 20;
+
+void losuj(int A[]){
+    for(int i=0; i<N; i++){
+        A[i] = rand() % 100;
+    }
+}
+
+void wypisz(int A[]){
+    for(int i=0; i<N; i++){
+        cout << A[i] << " ";
+    }
+}
+
+void quickSort(int A[], int p, int k){
+    if (p >= k) return;
+
+    int pivotIndex = rand() % (k - p + 1) + p;
+    swap(A[pivotIndex], A[k]);
+
+    int m = A[k];  
+    int i = p - 1;
+    for (int j = p; j < k; j++){
+        if (A[j] <= m){
+            i++;
+            swap(A[i], A[j]);
+        }
+    }
+
+    swap(A[i + 1], A[k]);  
+    int pivotPos = i + 1;
+
+    quickSort(A, p, pivotPos - 1);
+    quickSort(A, pivotPos + 1, k);
+}
+
+int main(){
+    int A[N];
+    int B[N];
+    srand(time(NULL));
+    losuj(A);
+    quickSort(A, 0, N - 1);
+    cout<<"Tablica A: ";
+    wypisz(A);
+    cout<<endl;
+
+    losuj(B);
+    quickSort(B, 0, N-1);
+    cout<<"tablica B: ";
+    wypisz(B);
+    cout<<endl;
+
+    cout<<"tablica AB: ";
+    int AB[N * 2];
+    int i = 0, j = 0, k = 0;
+
+    while (i < N && j < N) {
+        if (A[i] <= B[j]) {
+            AB[k++] = A[i++];
+        } else {
+            AB[k++] = B[j++];
+        }
+    }
+
+    while (i < N) AB[k++] = A[i++];
+    while (j < N) AB[k++] = B[j++];
+
+    for (int t = 0; t < 2 * N; t++)
+        cout << AB[t] << " ";
+
+    cout << endl;
+
+    return 0;
+}
 
 Zad. 7
 
