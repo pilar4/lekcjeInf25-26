19b9efd72ee419ad345e64c78014c3c8c0011fc1
Orla-cien <caderp08b@lo5.bielsko.pl>
2025-12-09 08:59:52 +0100

    Update 09-12.cpp

--- 09-12.cpp
+++ 09-12.cpp
@@ -348,6 +348,75 @@ int main(){
 }
 
 Zad. 7
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+const int N = 20;
+
+int sumaCyfr(int x) {
+    int s = 0;
+    while(x > 0){
+        s += x % 10;
+        x /= 10;
+    }
+    return s;
+}
+
+void losuj(int A[]){
+    for(int i = 0; i < N; i++){
+        A[i] = rand() % 100;  
+    }
+}
+
+void wypisz(int A[]){
+    for(int i = 0; i < N; i++){
+        cout << A[i] << " ";
+    }
+}
+
+void quickSort(int A[], int p, int k){
+    int i = p, j = k;
+    int pivot = A[(p + k) / 2];
+    int pivotSum = sumaCyfr(pivot);
+
+    while(i <= j){
+        while( (sumaCyfr(A[i]) < pivotSum) ||
+               (sumaCyfr(A[i]) == pivotSum && A[i] < pivot) )
+            i++;
+
+        while( (sumaCyfr(A[j]) > pivotSum) ||
+               (sumaCyfr(A[j]) == pivotSum && A[j] > pivot) )
+            j--;
+
+        if(i <= j){
+            swap(A[i], A[j]);
+            i++;
+            j--;
+        }
+    }
+
+    if(p < j) quickSort(A, p, j);
+    if(i < k) quickSort(A, i, k);
+}
+
+int main(){
+    int A[N];
+    srand(time(NULL));
+
+    losuj(A);
+    cout << "Tablica przed sortowaniem:\n";
+    wypisz(A);
+
+    cout << "\n\nTablica po sortowaniu wg sumy cyfr:\n";
+    quickSort(A, 0, N - 1);
+    wypisz(A);
+
+    cout << endl;
+    return 0;
+}
 
 Zad. 8
 
