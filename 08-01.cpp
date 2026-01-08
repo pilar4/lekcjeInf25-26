91055e54544c6cf6201e4e33eb524803cc697ca9
xyith <jonasz.stysiak@gmail.com>
2026-01-08 08:56:47 +0100

    Refactor content in 08-01.cpp

--- 08-01.cpp
+++ 08-01.cpp
@@ -1,1 +1,24 @@
-nigga balls
+cw 1
+
+ab+cd+*
+
+cw 2
+  
+  a. 
+    ab*c+
+  b. 
+    bc+a*
+  c.
+    ab+cd+*
+  d.
+    abc*+d-
+
+cw 3
+
+  a. 
+    ab+c*d-e*fg*+
+  b.
+    abc+*def+*-gh+/
+
+cw 4
+
