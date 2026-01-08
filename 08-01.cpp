51178930aa156b611d6f6241dcdaefcc3093fb3c
xyith <jonasz.stysiak@gmail.com>
2026-01-08 09:37:49 +0100

    Update 08-01.cpp

--- 08-01.cpp
+++ 08-01.cpp
@@ -208,3 +208,46 @@ int main(){
 
     return 0;
 }
+
+cw 7
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int Oblicz(string onp){
+    stack<int> stos; 
+    for(int i=0; i < onp.size(); i++) 
+        if (onp[i] >= '0' && onp[i] <= '9'){
+            stos.push(onp[i] - 48); 
+        }
+        else { 
+            int arg2 = stos.top(); stos.pop();
+            int arg1 = stos.top(); stos.pop(); 
+
+            switch (onp[i]) {
+                case '+': 
+                stos.push(arg1 + arg2); break; 
+
+                case '-':
+                stos.push(arg1 - arg2); break; 
+
+                case '*': 
+                stos.push(arg1 * arg2); break; 
+
+                case '/': 
+                stos.push(arg1 / arg2);
+            }
+        }
+    int wart = stos.top();
+    stos.pop();
+    return wart;
+}
+int main(){
+
+    string onp; cin>>onp;
+    cout<<Oblicz(onp);
+
+    return 0;
+}
