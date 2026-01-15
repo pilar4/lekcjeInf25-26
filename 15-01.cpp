9b487197d7be6c7b012e87efe3902ff34f7fe1cf
xyith <jonasz.stysiak@gmail.com>
2026-01-15 08:38:25 +0100

    Update 15-01.cpp

--- 15-01.cpp
+++ 15-01.cpp
@@ -108,6 +108,43 @@ int main()
 
 zad 3
 
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int oblicz(string &onp){
+    char znak = onp[onp.size()-1];
+    onp.erase(onp.size()-1,1);
+    if(znak>='0' && znak<='9'){
+        return znak-48;
+    }
+    else{
+        int arg2 = oblicz(onp);
+        int arg1 = oblicz(onp);
+        switch (znak){
+            case '+':
+                return arg1+arg2;
+            case '-':
+                return arg1-arg2;
+            case '*':
+                return arg1*arg2;
+            case '/':
+                return arg1/arg2;
+            case '%':
+                return arg1%arg2;
+        }
+    }
+}
+
+
+int main(){
+
+    string onp; cin>>onp;
+    cout<<oblicz(onp);
+
+    return 0;
+}
 
 zad 4
 
