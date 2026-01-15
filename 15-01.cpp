2822cb65e64310eed5e2a69fd0b69edda9f9e234
xyith <jonasz.stysiak@gmail.com>
2026-01-15 09:28:44 +0100

    Update 15-01.cpp

--- 15-01.cpp
+++ 15-01.cpp
@@ -166,3 +166,83 @@ int main (){
 
 
 zad 5
+
+zad 6
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+string ONP(string w)
+{
+
+    stack<char> stos; 
+    stos.push('#');
+    string onp = "";
+
+    for (int i = 0; i < w.size(); i++)
+    {
+        switch (w[i])
+        {
+            case '(':
+                stos.push('('); break;
+            case ')':
+                while(stos.top() != '(')
+                {
+                    onp = onp + stos.top(); stos.pop();
+                }
+                stos.pop(); break;
+            case '+':
+                while(stos.top() != '#' && stos.top() != '(')
+                {
+                    onp = onp + stos.top(); stos.pop();
+                }
+                stos.push('+'); break;
+            case '-':
+                while(stos.top() != '#' && stos.top() != '(')
+                {
+                    onp = onp + stos.top(); stos.pop();
+                }
+                stos.push('-'); break;
+            case '*':
+                if(stos.top() == '*' || stos.top() == '/' || stos.top() == '^')
+                {
+                    onp = onp + stos.top(); stos.pop(); 
+                }
+                stos.push('*'); break;
+            case '/':
+                if(stos.top() == '*' || stos.top() == '/' || stos.top() == '^')
+                {
+                    onp = onp + stos.top(); stos.pop(); 
+                }
+                stos.push('/'); break;
+            case '^':
+                if(stos.top() == '*' || stos.top() == '/' || stos.top() == '^')
+                {
+                    onp = onp + stos.top(); stos.pop(); 
+                }
+                stos.push('^'); break;
+            default: 
+                onp = onp + w[i];
+        }
+    }
+    while (stos.top() != '#')
+    {
+        onp = onp + stos.top(); 
+        stos.pop();
+    }
+
+    stos.pop();
+    return onp;
+}
+
+
+int main()
+{
+
+    string w; cin>>w;
+    cout<<ONP(w);
+
+    return 0;
+}
