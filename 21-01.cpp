33b5f7a3c8a26bbf20c1d11d099da380dad9834f
xyith <jonasz.stysiak@gmail.com>
2026-01-21 08:14:20 +0100

    Update 21-01.cpp

--- 21-01.cpp
+++ 21-01.cpp
@@ -53,3 +53,87 @@ int main(){
 
     return 0;
 }
+
+cw 2
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+const int N=10;
+
+void WczytajLabirynt(int Lab[][N]){
+    string s;
+    ifstream we("labirynt-txt.txt");
+    for(int i=0;i<N;i++){
+        we>>s;
+        for(int j=0;j<N;j++){
+            if(s[j]=='X'){
+                Lab[i][j]=-1;
+            }
+            else{
+                Lab[i][j]=0;
+            }
+        }
+    }
+    we.close();
+}
+
+void WypiszLabirynt(int Lab[][N]){
+    cout<<"   ";
+    for(int j=0;j<N;j++){
+        cout<<setw(3)<<j;
+    }
+    cout<<endl;
+    for(int i=0;i<N;i++){
+        cout<<setw(3)<<i;
+        for(int j=0;j<N;j++){
+            if(Lab[i][j]==-1){
+                cout<<"  X";
+            }
+            else{
+                cout<<"   ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
+bool droga(int Lab[N][N],int w,int k){
+    Lab[w][k] = 1;
+    if(w==0 || w==N-1 || k==0 || k==N-1){
+        return true;
+    }
+    if(Lab[w-1][k]==0 && droga(Lab,w-1,k)){
+        return true;
+    }
+    if(Lab[w+1][k]==0 && droga(Lab,w+1,k)){
+        return true;
+    }
+    if(Lab[w][k-1]==0 && droga(Lab,w,k-1)){
+        return true;
+    }
+    if(Lab[w][k+1]==0 && droga(Lab,w,k+1)){
+        return true;
+    }
+    return false;
+}
+
+int main(){
+    int Lab[N][N];
+    int w,k;
+    cin>>w>>k;
+
+    WczytajLabirynt(Lab);
+    WypiszLabirynt(Lab);
+
+    if(droga(Lab,w,k)){
+        cout<<"TAK"<<endl;
+    }
+    else cout<<"NIE";
+
+    return 0;
+}
