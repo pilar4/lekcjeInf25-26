b7c23b6a68758040465747e119b3da63d696ba93
Orla-cien <caderp08b@lo5.bielsko.pl>
2026-01-28 08:20:11 +0100

    Update 28-01.cpp

--- 28-01.cpp
+++ 28-01.cpp
@@ -41,4 +41,80 @@ int main(){
 }
 cw3
 
+
 cw4
+#include <iostream>
+#include <string>
+#include <list>
+#include <fstream>
+#include <queue>
+
+using namespace std;
+
+const string alfabet="abcdefghijklmnoprstuvwxyz";
+const int N = alfabet.size();
+
+
+void BudujListe(list<string> &lista, int &mdl)
+{
+    ifstream we("slowa.txt");
+    string s;
+    mdl=0;
+    while (we>>s)
+    {
+        lista.push_back(s);
+        if(s.size()>mdl) mdl=s.size();
+    }
+    we.close();
+}
+
+void ZapiszListe(list<string> lista)
+{
+    ofstream wy("slowa.txt");
+    list<string>::iterator it;
+    for(it=lista.begin();it!=lista.end();it++)
+        wy<<*it<<endl;
+    wy.close();
+}
+
+void SortujSlowa(list<string> &lista, int mdl)
+{
+    queue<string> Kubelki[N];
+    list<string>::iterator it;
+    int i, j;
+    string s;
+    for (i=mdl-1;i>=0;i--)
+    {
+        it=lista.begin();
+        while (it!=lista.end())
+        {
+            s=*it;
+            if (s.size()>i)
+            {
+                it=lista.erase(it);
+                j=alfabet.find(s[i]);
+                Kubelki[j].push(s);
+            }
+            else it++;
+        }
+        for (j=0;j<N;j++)
+            while (!Kubelki[j].empty())
+            {
+                lista.push_back(Kubelki[j].front());
+                Kubelki[j].pop();
+            }
+    }
+}
+
+int main()
+{
+    list<string> lista;
+    int mdl;
+
+    BudujListe(lista, mdl);
+    SortujSlowa(lista, mdl);
+    ZapiszListe(lista);
+
+    return 0;
+}
+
