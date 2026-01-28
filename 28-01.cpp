3ac83551dcdf21c5910b7120aa3408752ebb11bb
karoluxxx <karol.firnis@gmail.com>
2026-01-28 07:43:09 +0100

    Update 28-01.cpp

--- 28-01.cpp
+++ 28-01.cpp
@@ -1,4 +1,13 @@
 cw1
+Problem Flawiusza dla 7 os co 3 wstaje :
+-Wszyscy siedzą przy stole numerki od 1 do 7
+-Wstaje kolega nr. 3 
+-Wstaje nr.6
+-Wstaje ziomek nr.2
+-Opuszcza stół nr.7
+-Wychodzi nr.5
+-Żegna nas nr.1
+-Na koniec zostaje nr.4
 
 cw2
 
