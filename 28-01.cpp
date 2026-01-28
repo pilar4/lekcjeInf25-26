207cd26dea1164b843566ccfde20270f2bf3d6ac
pilar4 <112971080+pilar4@users.noreply.github.com>
2026-01-28 07:55:01 +0100

    Add elimination game logic using STL list
    Implement a list-based elimination game where elements are removed based on user-defined steps.

--- 28-01.cpp
+++ 28-01.cpp
@@ -10,7 +10,35 @@ Problem Flawiusza dla 7 os co 3 wstaje :
 -Na koniec zostaje nr.4
 
 cw2
+#include<iostream>
+#include<list>
+using namespace std;
 
+
+int main(){
+    list<int> lista;
+    list<int>::iterator it;
+    int n, k;
+    cout<<"n "; cin>>n;
+    cout<<"k "; cin>>k;
+    for(int i=1;i<=n;i++){
+        lista.push_back(i);
+    }
+    it=lista.begin();
+    for(int i=1;i<n;i++){
+        for(int j=1;j<k;j++){
+            it++;
+            if(it==lista.end()) it=lista.begin();
+        }
+        cout<<"krok "<<i<<": usuniety element ";
+        cout<<*it<<endl;
+        it=lista.erase(it);
+        if(it==lista.end()) it=lista.begin();
+    }
+    cout<<"ostatni element: "<<*it<<" ";
+    return 0;
+
+}
 cw3
 
 cw4
