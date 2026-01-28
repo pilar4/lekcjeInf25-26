ccc5d63c9a3d7e3f5028c5f87e243221de0499dc
xyith <jonasz.stysiak@gmail.com>
2026-01-28 07:53:02 +0100

    Create copycat.js

---
+++ copycat.js
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <list>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void bucketSort(list<string>& words) {
+    // znajdź maksymalną długość słowa
+    size_t maxLen = 0;
+    for (const auto& w : words) {
+        if (w.length() > maxLen)
+            maxLen = w.length();
+    }
+
+    // sortowanie od ostatniego znaku do pierwszego
+    for (int pos = maxLen - 1; pos >= 0; pos--) {
+        vector<list<string>> buckets(27); 
+        // 0 -> brak znaku (krótsze słowa)
+        // 1-26 -> litery 'a' - 'z'
+
+        for (const auto& word : words) {
+            if (pos < word.length()) {
+                int index = word[pos] - 'a' + 1;
+                buckets[index].push_back(word);
+            } else {
+                buckets[0].push_back(word);
+            }
+        }
+
+        // scal kubełki z powrotem do listy
+        words.clear();
+        for (auto& bucket : buckets) {
+            words.splice(words.end(), bucket);
+        }
+    }
+}
+
+int main() {
+    list<string> words = {
+        "krzeslo", "krzak", "krem", "kres",
+        "kraina", "kret", "kraksa", "kredyt"
+    };
+
+    bucketSort(words);
+
+    for (const auto& w : words) {
+        cout << w << endl;
+    }
+
+    return 0;
+}
