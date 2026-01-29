94438f3035ad5bd44c247c36926c005e3dec7584
pilar4 <112971080+pilar4@users.noreply.github.com>
2026-01-29 09:27:15 +0100

    Update 29-01.cpp

--- 29-01.cpp
+++ 29-01.cpp
@@ -1,1 +1,40 @@
-kirk
+zad 3
+#include<iostream>
+#include<random>
+using namespace std;
+
+const int N = 10;
+int tab[N];
+
+void losowanie(){
+    srand(time(0));
+    for(int i=0;i<N;i++){
+        tab[i]=rand()%19-9;
+    }
+}
+
+void wypisz(){
+    cout<<endl;
+    for(int i=0;i<N;i++)
+        cout<<tab[i]<<" ";
+}
+
+void bubble_sort(){
+    for(int i = 0; i < N - 1; i++){
+        for(int j = 0; j < N - 1 - i; j++){
+            if (abs(tab[j]) > abs(tab[j+1])) {
+                swap(tab[j], tab[j+1]);
+            }
+        }
+    }
+}
+
+
+
+int main(){
+    losowanie();
+    wypisz();
+    bubble_sort();
+    wypisz();
+
+}
