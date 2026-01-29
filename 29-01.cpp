c03ff2cb4d38626d759f4b2b5b35a593d6f27b5d
pilar4 <112971080+pilar4@users.noreply.github.com>
2026-01-29 09:29:29 +0100

    Implement random number generation and bubble sort

--- 29-01.cpp
+++ 29-01.cpp
@@ -38,3 +38,45 @@ int main(){
     wypisz();
 
 }
+
+
+zad 5
+#include<iostream>
+#include<random>
+using namespace std;
+
+const int N = 10;
+int tab[N];
+
+void losowanie(){
+    srand(time(0));
+    for(int i=0;i<N;i++){
+        tab[i]=rand()%100;
+    }
+}
+
+void wypisz(){
+    cout<<endl;
+    for(int i=0;i<N;i++)
+        cout<<tab[i]<<" ";
+}
+
+void bubble_sort(){
+    for(int i = 0; i < N - 1; i++){
+        for(int j = 0; j < N - 1 - i; j++){
+            if ((tab[j]%10) > (tab[j+1]%10)) {
+                swap(tab[j], tab[j+1]);
+            }
+        }
+    }
+}
+
+
+
+int main(){
+    losowanie();
+    wypisz();
+    bubble_sort();
+    wypisz();
+
+}
