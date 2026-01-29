b7d65c158819fe9fa6ab7c1855007b07d5dfefb5
Orla-cien <caderp08b@lo5.bielsko.pl>
2026-01-29 09:36:40 +0100

    Update 29-01-grafy.cpp

--- 29-01-grafy.cpp
+++ 29-01-grafy.cpp
@@ -1,1 +1,44 @@
+Ćwiczenie 1
+Było na lekcji
+
+Ćwiczenie 2
+Było na lekcji
+
+Ćwiczenie 3
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Podaj rozmiar: ";
+    cin>>n;
+
+    vector<int> v(n);
+    srand(time(NULL));
+    for(int i=0; i<n; i++)
+    {
+        v[i]=rand()%100;
+    }
+
+    cout<<"Wektor: "<<endl;
+    for(int i=0; i<n; i++)
+    {
+        cout<<v[i]<<" ";
+    }
+
+    return 0;
+}
+
+
+Ćwiczenie 4
+Ćwiczenie 5
+Ćwiczenie 6
+Ćwiczenie 7
+Ćwiczenie 8
+
 
