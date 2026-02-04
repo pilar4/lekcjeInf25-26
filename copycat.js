cad86df45b10a5ce9732e64cc72699f7e2caafa5
xyith <jonasz.stysiak@gmail.com>
2026-02-04 07:51:08 +0100

    Update copycat.js

--- copycat.js
+++ copycat.js
@@ -1,28 +1,13 @@
-void BFS(int w1, tgraf Graf)
+void Czytaj(tgraf &Graf)
 {
-    int w2;
-    vector<bool> Odwiedzone;
-    Odwiedzone.resize(Graf.size(), false);
-    queue<int> wierzch;
-
-    wierzch.push(w1);
-    Odwiedzone[w1] = true;
-
-    while (!wierzch.empty())
+    int n, m, w1, w2;
+    ifstream we("graf.txt");
+    we >> n >> m;
+    Graf.resize(n);
+    for (int i = 0; i < m; i++)
     {
-        w1 = wierzch.front();
-        wierzch.pop();
-
-        cout << "Odwiedzony wierzcholek: " << w1 << endl;
-
-        for (int i = 0; i < Graf[w1].size(); i++)
-        {
-            w2 = Graf[w1][i];
-            if (!Odwiedzone[w2])
-            {
-                wierzch.push(w2);
-                Odwiedzone[w2] = true;
-            }
-        }
+        we >> w1 >> w2;
+        Graf[w1].push_back(w2);
     }
+    we.close();
 }
