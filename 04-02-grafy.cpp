71b038bd6bb8e17b4a104ed311b02b7735ce4876
karoluxxx <karol.firnis@gmail.com>
2026-02-04 07:55:41 +0100

    Update 04-02-grafy.cpp

--- 04-02-grafy.cpp
+++ 04-02-grafy.cpp
@@ -68,6 +68,53 @@ int main()
 
 Ćwiczenie 5
 
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+typedef vector<vector<int> > tgraf;
+
+void czytaj(tgraf &Graf){
+    int n, m, w1, w2;
+    ifstream we("graf.txt");
+    we>>n>>m;
+    Graf.resize(n);
+    for(int i=0;i<m;i++){
+        we>>w1>>w2;
+        Graf[w1].push_back(w2);
+    }
+    we.close();
+}
+
+void DFS(int w1, tgraf &Graf, vector<bool> &Odwiedzone){
+    cout<<"odw wierz: "<<w1<<endl;
+    Odwiedzone[w1]=true;
+
+    for(int i=0;i<Graf[w1].size();i++){
+        int w2=Graf[w1][i];
+        if(!Odwiedzone[w2]){
+            DFS(w2,Graf,Odwiedzone);
+        }
+    }
+}
+
+int main()
+{
+    tgraf Graf;
+    czytaj(Graf);
+
+    vector<bool> Odwiedzone;
+    Odwiedzone.resize(Graf.size(),false);
+
+    int w1; cin>>w1;
+
+    DFS(w1,Graf,Odwiedzone);
+    return 0;
+}
+
+
 Ćwiczenie 6
 Ćwiczenie 7
 Ćwiczenie 8
