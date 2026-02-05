57296354b163cc5cab47b0c0b67ca04e61b9c582
pilar4 <112971080+pilar4@users.noreply.github.com>
2026-02-05 09:08:57 +0100

    Update 05-02.cpp

--- 05-02.cpp
+++ 05-02.cpp
@@ -106,3 +106,85 @@ int main()
     cout<<"Nie istnieje droga ";
     return 0;
 }
+
+zad 5
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+typedef vector<vector<int> > tgraf;
+
+int czytaj(tgraf &Graf){
+    int n, m, w1, w2;
+    ifstream we("graf_2.txt");
+    we>>n>>m;
+    Graf.resize(n);
+    for(int i=0;i<m;i++){
+        we>>w1>>w2;
+        Graf[w1].push_back(w2);
+    }
+    we.close();
+
+    return n;
+}
+
+stack<int> s;
+
+void DFS(int w1, tgraf &Graf, vector<bool> &Odwiedzone){
+    s.push(w1);
+    Odwiedzone[w1]=true;
+
+    for(int i=0;i<Graf[w1].size();i++){
+        int w2=Graf[w1][i];
+        if(!Odwiedzone[w2]){
+            DFS(w2,Graf,Odwiedzone);
+        }
+    }
+
+}
+
+int main()
+{
+    tgraf Graf;
+    int num = czytaj(Graf);
+
+    vector<bool> Odwiedzone;
+    Odwiedzone.resize(Graf.size(),false);
+
+    int w1 = 0;
+
+    vector<bool> vec;
+    for(int i=0;i<num;i++){
+        vec.push_back(0);
+    }
+
+
+    DFS(w1, Graf, Odwiedzone);
+    for(int i=0;i<num;i++){
+        vec[s.top()]=1;
+        s.pop();
+    }
+    for(int i=0;i<num;i++){
+        if(vec[i]==0){
+            cout<<" Nie jest spojny";
+            return 0;
+        }
+    }
+    cout<<"Jest spojny ";
+    return 0;
+
+    /*
+    6 9
+0 3
+1 0
+1 2
+1 4
+2 0
+2 5
+3 1
+3 5
+4 5
+    */
+}
