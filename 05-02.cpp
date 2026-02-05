867e4782219b399986e4e4fff2dc46a36b5c2ab7
pilar4 <112971080+pilar4@users.noreply.github.com>
2026-02-05 08:47:46 +0100

    Update 05-02.cpp

--- 05-02.cpp
+++ 05-02.cpp
@@ -44,3 +44,65 @@ int main()
    DFS(w1,Graf,Odwiedzone);
    return 0;
 }
+
+
+zad 2
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+typedef vector<vector<int> > tgraf;
+
+int czytaj(tgraf &Graf){
+    int n, m, w1, w2;
+    ifstream we("graf_2.txt");
+    we>>n>>m;
+    Graf.resize(n);
+    for(int i=0;i<m;i++){
+        we>>w1>>w2;
+        Graf[w1].push_back(w2);
+    }
+    we.close();
+
+    return n;
+}
+
+stack<int> s;
+
+void DFS(int w1, tgraf &Graf, vector<bool> &Odwiedzone){
+    s.push(w1);
+    Odwiedzone[w1]=true;
+
+    for(int i=0;i<Graf[w1].size();i++){
+        int w2=Graf[w1][i];
+        if(!Odwiedzone[w2]){
+            DFS(w2,Graf,Odwiedzone);
+        }
+    }
+
+}
+
+int main()
+{
+    tgraf Graf;
+    int num = czytaj(Graf);
+
+    vector<bool> Odwiedzone;
+    Odwiedzone.resize(Graf.size(),false);
+
+    int w1 = 0;
+
+
+    DFS(w1, Graf, Odwiedzone);
+    for(int i=0;i<num;i++){
+        if(s.top()==num-1){
+            cout<<"Istnieje droga ";
+            return 0;
+        }
+        s.pop();
+    }
+    cout<<"Nie istnieje droga ";
+    return 0;
+}
