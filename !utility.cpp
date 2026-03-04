b6b3155dee20d6d653565b9fe14de34352f42899
Orla-cien <caderp08b@lo5.bielsko.pl>
2026-03-04 07:55:19 +0100

    Update !utility.cpp

--- !utility.cpp
+++ !utility.cpp
@@ -1,63 +1,31 @@
-const int N = 5
-
-void Losuj (int A[] [N + 1] rangle
-
+int main()
 {
-
-for (int i = 0; i < N ;i++)
-
-for (int j = 0; j < N ;j++) A[i][j]=1+rand()%9;
-
-for (int i = 0 i<N;i++)
-
-{
-
-A[N][i] = 0 A[i][N] = emptyset
-
+    int licz, mian, i, j;
+    cout<<"Licznik: " cin>>licz;
+    cout<<"Mianownik: " cin>>mian;
+    string s="0,";
+    bool jest_okres=false;
+    wymierna w;
+    vector<wymierna> Tab;
+    while (!jest_okres)
+    {
+        licz=licz*2;
+        w.calk=licz/mian;
+        licz=licz%mian;
+        w.licz=licz;
+        i=0;
+        while(i<Tab.size() && !jest_okres)
+            if(Tab[i]calk==w.calk && Tab[i].licz==w.licz)
+        else i++;
+    if (!jest_okres) Tab.push_back(w);
+    }
+    for (j=0;j<Tab.size();j++)
+    {
+        if (j==i) s=s+'(';
+        if (Tab[j].calk==1) s=s+'1';
+        else s=s+'0';
+    }
+    s=s+')';
+    cout<<"Rozwiniecie binarne: "<<s;
+    return 0;
 }
-
-}
-
-void Wypisz(int A[] [N + 1] )
-
-{
-
-for (int i = 0 i<N;i++)
-
-{
-
-for (int j = 0; j < N ;j++) cout<<A[i][j]<<" ";
-
-cout<<endl;
-
-}
-
-}
-
-int MaksSuma(int A[] [N + 1] )
-
-{
-
-int suma=A[0][0], w = 0 k = 0
-
-for (int i = 1; i < 2N - 1 ;i++)
-
-if (A[w + 1][k] > A[w][k + 1])
-
-{
-
-suma+=A[w+1] [k]; w++;
-
-}
-
-else
-
-{
-
-suma+=A[w] [k+1]; k++;
-
-}
-
-return suma;
-
-228
