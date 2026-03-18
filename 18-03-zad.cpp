c8fd2e9f5556f667e39f79e1b96bce533bd80398
karoluxxx <karol.firnis@gmail.com>
2026-03-18 08:16:39 +0100

    Update 18-03-zad.cpp

--- 18-03-zad.cpp
+++ 18-03-zad.cpp
@@ -1,2 +1,44 @@
 zad1
 0,00156
+zad2
+  #include <iostream>
+#include <iomanip>
+using namespace std;
+
+const float EPS = 0.0000001;
+
+int main() { float a, b, c, delta, pdelta, x1, x2;
+
+cout << "a = "; cin >> a;
+cout << "b = "; cin >> b;
+cout << "c = "; cin >> c;
+
+delta = b*b - 4*a*c;
+
+if (delta < 0){
+    cout << "Brak pierwiastkow";
+}
+else if (delta=0)
+{
+    (-b)/(2*a);
+}
+else
+{
+    pdelta = sqrt(delta);
+
+    if (b > 0)
+        x1 = (-b - pdelta) / (2 * a);
+    else
+        x1 = (-b + pdelta) / (2 * a);
+
+    if (abs(x1) > EPS)
+        x2 = c / (a * x1);
+    else
+        x2 = 0;
+
+    cout << "x1 = " << x1 << endl;
+    cout << "x2 = " << x2;
+}
+
+return 0;
+}
