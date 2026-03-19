7adb4bbf68be2139746c8fd6cd5817fee1bf4a0a
pilar4 <112971080+pilar4@users.noreply.github.com>
2026-03-19 09:11:09 +0100

    Update 19-03.cpp

--- 19-03.cpp
+++ 19-03.cpp
@@ -42,6 +42,45 @@ int main()
 }
 
 cw 2
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <cmath>
+#include <iomanip>
+#include <algorithm>
+#include <fstream>
+#include <stack>
+#include <queue>
+#include <list>
+#include <vector>
+
+using namespace std;
+
+void czytaj(vector<float> &A){
+    int n;
+    cout<<"stopien wielomianu";
+    cin>>n;
+    A.resize(n+1);
+    for(int i=n;i>=0;i--){
+        cout<<"a"<<i<<" = ";
+        cin>>A[i];
+    }
+}
+
+float W(vector<float> A, float  x){
+    int n=A.size()-1;
+    float y=A[n];
+    for(int i=n-1;i>=0;i--)
+        y=x*y+A[i];
+    return y;
+}
+
+int main(){
+    vector<float> a;
+    czytaj(a);
+    float x;
+    cin>>x;
+    cout<<W (a,x);
+}
 cw 3
-cw 4
-cw 5
+
