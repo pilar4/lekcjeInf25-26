20838e08474836d02fd5aa7370b4c17ee1c02e5c
Orla-cien <caderp08b@lo5.bielsko.pl>
2026-03-19 08:47:56 +0100

    Update 19-03.cpp

--- 19-03.cpp
+++ 19-03.cpp
@@ -1,5 +1,47 @@
 cw 1
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+float W(vector<float> A, float x)
+{
+    float y = A[0], z = 1;
+
+    for (int i = 1; i < A.size(); i++)
+    {
+        z = z * x;       
+        y = y + A[i] * z;
+    }
+
+    return y;
+}
+
+int main()
+{
+    int n;
+    cout << "Podaj stopien wielomianu: ";
+    cin >> n;
+
+    vector<float> A(n + 1);
+
+    cout << "Podaj wspolczynniki od A0 do A" << n << ":\n";
+    for (int i = 0; i <= n; i++)
+        cin >> A[i];
+
+    float x;
+    cout << "Podaj x: ";
+    cin >> x;
+
+    float wynik = W(A, x);
+
+    cout << "Wartosc wielomianu = " << wynik << endl;
+
+    return 0;
+}
+
 cw 2
-  cw 3
-      cw 4
-          cw 5
+cw 3
+cw 4
+cw 5
