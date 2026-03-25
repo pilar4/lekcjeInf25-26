74f8bb25db2d5c531fb8d0c8ee43211de0df5b34
pilar4 <112971080+pilar4@users.noreply.github.com>
2026-03-25 08:30:39 +0100

    Update 25-03-cw.cpp

--- 25-03-cw.cpp
+++ 25-03-cw.cpp
@@ -1,1 +1,27 @@
 cw1
+cw5
+#include <iostream>
+#include <vector>
+using namespace std;
+
+float f(float x){
+    return 1.0/6*x*x*x-x*x+0.5*x+5.0/0.3;
+}
+
+int main()
+{
+    float a, b, x, dx, s=0;
+    int n;
+    cout<<"A = "; cin>>a;
+    cout<<"B = "; cin>>b;
+    cout<<"N = "; cin>>n;
+    dx=(b-a)/n;
+    x=a;
+    for(int i=1;i<=n;i++){
+        x=x+dx;
+        s=s+dx*f(x);
+    }
+    cout<<"Pole obszaru "<<s;
+    cout<<endl;
+    return 0;
+}
