3b7791034b1fb28e6354a2e5f3c4c5d22c9e6ec9
Orla-cien <caderp08b@lo5.bielsko.pl>
2026-03-25 08:31:15 +0100

    Update 25-03-cw.cpp

--- 25-03-cw.cpp
+++ 25-03-cw.cpp
@@ -1,4 +1,112 @@
-cw1
+ćw.1
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const float EPS = 0.0001;
+
+float f(float x)
+{
+    return x*x - 4;
+}
+
+int main()
+{
+    float a, b, x0, fa, fb, fx0;
+
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+
+    fa = f(a);
+    fb = f(b);
+
+    if (fa * fb >= 0)
+    {
+        cout << "Wartosci funkcji dla koncow przedzialu ";
+        cout << "nie maja roznych znakow" << endl;
+        return 0;
+    }
+
+    x0 = (a + b) / 2;
+    fx0 = f(x0);
+
+    while (abs(fx0) > EPS && (b - a) > EPS)
+    {
+        if (fa * fx0 < 0)
+        {
+            b = x0;
+            fb = fx0;
+        }
+        else
+        {
+            a = x0;
+            fa = fx0;
+        }
+        x0 = (a + b) / 2;
+        fx0 = f(x0);
+    }
+    cout << "x0 = " << x0 << endl;
+    return 0;
+}
+
+ćw.2
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const double EPS = 0.0001;
+
+double f(double t, double x)
+{
+    return t * t - x;
+}
+
+int main()
+{
+    double x;
+    cout << "Podaj liczbe x > 1: ";
+    cin >> x;
+
+    if (x <= 1)
+    {
+        cout << "`niepoprawna liczba" << endl;
+        return 0;
+    }
+
+    double a = 1;
+    double b = x;
+    double x0, fa, fb, fx0;
+
+    fa = f(a, x);
+    fb = f(b, x);
+
+    x0 = (a + b) / 2;
+    fx0 = f(x0, x);
+
+    while (abs(fx0) > EPS && (b - a) > EPS)
+    {
+        if (fa * fx0 < 0)
+        {
+            b = x0;
+            fb = fx0;
+        }
+        else
+        {
+            a = x0;
+            fa = fx0;
+        }
+
+        x0 = (a + b) / 2;
+        fx0 = f(x0, x);
+    }
+
+    cout << "Przyblizony pierwiastek = " << x0 << endl;
+
+    return 0;
+}
+
 cw5
 #include <iostream>
 #include <vector>
