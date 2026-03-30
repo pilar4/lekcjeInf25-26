5f0086aefe2aeb6ca1658de2b67a24bfa9e1b725
xyith <jonasz.stysiak@gmail.com>
2026-03-30 13:09:30 +0200

    Update 30-03-zad.cpp

--- 30-03-zad.cpp
+++ 30-03-zad.cpp
@@ -49,7 +49,43 @@ int main()
     cout << "x0 = " << x0 << endl;
     return 0;
 }
+zad 2
 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double sqrt_bisekcja(double c, double E = 1e-6) {
+    double a = 0.0;
+    double b = 1.0;
+    double m;
+
+    while ((b - a) / 2 > E) {
+        m = (a + b) / 2;
+
+        if (m * m - c == 0) {
+            return m;
+        } else if (m * m - c < 0) {
+            a = m;
+        } else {
+            b = m;
+        }
+    }
+
+    return (a + b) / 2;
+}
+
+int main() {
+    double c;
+    cout << "Podaj liczbe z przedzialu (0,1): ";
+    cin >> c;
+
+    double wynik = sqrt_bisekcja(c);
+    cout << "pierwiastek z " << c << " ≈ " << wynik << endl;
+
+    return 0;
+}
 
 zad4
 P ≈ 2.98
