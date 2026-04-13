8d73e0b6fea9b57b27dd8b171bf88407f94b014c
xyith <jonasz.stysiak@gmail.com>
2026-04-13 13:31:12 +0200

    Update !.@gerigere.cpp

--- !.@gerigere.cpp
+++ !.@gerigere.cpp
@@ -1,18 +1,17 @@
-bool PrzecOdcinki(punkt A, punkt B, punkt C, punkt D)
+float det(float x1, float y1,
+          float x2, float y2,
+          float x3, float y3)
 {
-    float w1, w2, w3, w4;
+    return (y3 - y1) * (x2 - x1) - (y2 - y1) * (x3 - x1);
+}
 
-    w1 = det(A.x, A.y, B.x, B.y, C.x, C.y);
-    w2 = det(A.x, A.y, B.x, B.y, D.x, D.y);
-    w3 = det(C.x, C.y, D.x, D.y, A.x, A.y);
-    w4 = det(C.x, C.y, D.x, D.y, B.x, B.y);
+bool PktWOdcinku(punkt A, punkt B, punkt P)
+{
+    float w = det(A.x, A.y, B.x, B.y, P.x, P.y);
+    
+    if (abs(w) > EPS)
+        return false;
 
-    if (w1 * w2 < 0 && w3 * w4 < 0) return true;
-
-    if (PktWodcinku(A, B, C)) return true;
-    if (PktWodcinku(A, B, D)) return true;
-    if (PktWodcinku(C, D, A)) return true;
-    if (PktWodcinku(C, D, B)) return true;
-
-    return false;
+    return P.x >= min(A.x, B.x) && P.x <= max(A.x, B.x) &&
+           P.y >= min(A.y, B.y) && P.y <= max(A.y, B.y);
 }
