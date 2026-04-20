a365f6590c63a428e176cbfa009f57bf49a7e109
Orla-cien <caderp08b@lo5.bielsko.pl>
2026-04-20 13:29:59 +0200

    Update 20-04.cpp

--- 20-04.cpp
+++ 20-04.cpp
@@ -1,4 +1,22 @@
 cw 1
+function setup() {
+  createCanvas(600, 400);
+}
+function Cantor(n,x,y,d){
+  if (n==0){
+    line(x,y,x+d,y);
+  }
+  else{
+    Cantor(n-1,x,y,d/3);
+    Cantor(n-1,x+2*d/3,y,d/3);
+  }
+}
+
+function draw() {
+  background(220);
+  Cantor(3,10,200,580);
+  noLoop();
+}
 cw 2
 function setup() {
   createCanvas(600, 400);
