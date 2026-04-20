9fac4c05dea68d177e39593717e577ae24b39069
pilar4 <112971080+pilar4@users.noreply.github.com>
2026-04-20 13:55:18 +0200

    Update 20-04.js

--- 20-04.js
+++ 20-04.js
@@ -127,3 +127,26 @@ function draw() {
   background(220);
   DrzewoBinarne(6, 300, 500, 100, PI / 2);
 }
+
+cw6
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+function Sierpinski(n,x,y,a){
+  if(n==0){
+    square(x,y,a);
+  }
+  else {
+    var b=a/3,i;
+    for (i=0;i<3;i++) {Sierpinski(n-1,x+i*b,y,b)}
+    for (i=0;i<2;i++) {Sierpinski(n-1,x+2*i*b,y+b,b)}
+    for (i=0;i<3;i++) {Sierpinski(n-1,x+i*b,y+2*b,b)}
+  }
+}
+
+function draw() {
+  background(220);
+  Sierpinski(3,0,0,400);
+}
