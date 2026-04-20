6e9f1e916ec55fa22df225811fbe4339fc0efe91
pilar4 <112971080+pilar4@users.noreply.github.com>
2026-04-20 13:04:32 +0200

    Create 20-4.cpp

---
+++ 20-4.cpp
@@ -0,0 +1,22 @@
+cw 1
+cw 2
+function setup() {
+  createCanvas(600, 400);
+}
+
+function Cantor(n,x,y,d){
+  if(n==0){
+    line(x,y,x+d,y);
+  } else {
+    Cantor(n-1,x,y,d/3);
+    Cantor(n-1,x+2*d/3,y,d/3);
+  }
+}
+
+function draw() {
+  background(220);
+  for (n=0;n<7;n++){
+    Cantor(n,10,(n+1)*50,580);
+  }
+}
+
