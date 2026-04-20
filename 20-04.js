ffb2bff76ab00d0b075c5151456c4e07b702dc88
Orla-cien <caderp08b@lo5.bielsko.pl>
2026-04-20 13:49:31 +0200

    Update 20-04.js

--- 20-04.js
+++ 20-04.js
@@ -66,6 +66,38 @@ function draw() {
     Koch(4,50,300,500,0);
     noLoop();
 }
+cw 4
+function Koch(n, x, y, d, alfa){
+    var dx = d * cos(alfa);
+    var dy = d * sin(alfa);
+
+    if(n == 0){
+        line(x, y, x + dx, y - dy);
+    }
+    else{
+        Koch(n - 1, x, y, d / 3, alfa);
+        Koch(n - 1, x + dx / 3, y - dy / 3, d / 3, alfa + PI / 3);
+        Koch(n - 1, x + dx / 2 - dy * p3 / 6, y - dy / 2 - dx * p3 / 6, d / 3, alfa - PI / 3);
+        Koch(n - 1, x + 2 * dx / 3, y - 2 * dy / 3, d / 3, alfa);
+    }
+}
+
+function PlatekKocha(n, xs, ys, d){
+    Koch(n, xs - d / 2, ys + d * p3 / 6, d, PI / 3);
+    Koch(n, xs, ys - d * p3 / 3, d, -PI / 3);
+    Koch(n, xs + d / 2, ys + d * p3 / 6, d, PI);
+}
+
+function setup(){
+    createCanvas(800, 800);
+    p3 = sqrt(3);
+}
+
+function draw(){
+    background(220);
+    PlatekKocha(1000, 400, 400, 600);
+    noLoop();
+}
 
 cw5
 function setup() {
