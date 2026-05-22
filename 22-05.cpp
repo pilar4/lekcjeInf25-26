ae40b88e65b09ad8a0790a68ff8e16bcb746a414
xyith <jonasz.stysiak@gmail.com>
2026-05-22 08:02:17 +0200

    Update 22-05.cpp

--- 22-05.cpp
+++ 22-05.cpp
@@ -35,3 +35,54 @@ int main(){
     cout<<"Klucz prywatny: "<<s<<" "<<y<<endl;
     return 0;
 }
+cw 4(chb)
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int RSA(int podst, int wykl, int n)
+{
+    int w = 1;
+
+    while (wykl > 0)
+    {
+        if (wykl % 2 == 1)
+            w = (w * podst) % n;
+
+        wykl = wykl / 2;
+
+        if (wykl > 0)
+            podst = (podst * podst) % n;
+    }
+
+    return w;
+}
+
+int main()
+{
+    ifstream we("t_jawny_RSA.txt");
+    ofstream wy("szyfrogram_RSA.txt");
+
+    unsigned char ch;
+    int e, n;
+
+    cout << "Podaj klucz publiczny:" << endl;
+    cout << "e = ";
+    cin >> e;
+    cout << "n = ";
+    cin >> n;
+
+    we >> noskipws;
+
+    while (we >> ch)
+        wy << RSA(ch, e, n) << " ";
+
+    we.close();
+    wy.close();
+
+    cout << "Tekst zostal zaszyfrowany";
+
+    return 0;
+}
