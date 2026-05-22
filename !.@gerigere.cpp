0ded19a81f52e0f4545d3a2feb409b36fa67a59f
xyith <jonasz.stysiak@gmail.com>
2026-05-22 07:48:27 +0200

    Update !.@gerigere.cpp

--- !.@gerigere.cpp
+++ !.@gerigere.cpp
@@ -1,54 +1,44 @@
-const int N = 997; // liczba pierwsza
-const int M = 26;  // długość alfabetu
-
-int Potega(int podst, int wykl)
+int RSA(int podst, int wykl, int n)
 {
     int w = 1;
+
     while (wykl > 0)
     {
-        if (wykl % 2 == 1) w = (w * podst) % N;
+        if (wykl % 2 == 1)
+            w = (w * podst) % n;
+
         wykl = wykl / 2;
-        if (wykl > 0) podst = (podst * podst) % N;
+
+        if (wykl > 0)
+            podst = (podst * podst) % n;
     }
+
     return w;
 }
 
-int Hash0(string s)
+int main()
 {
-    int w = 0;
-    for (int i = 0; i < s.size(); i++)
-        w = ((w * M) % N + s[i] - 'a') % N;
-    return w;
-}
+    ifstream we("t_jawny_RSA.txt");
+    ofstream wy("szyfrogram_RSA.txt");
 
-int Hash1(int h0, int jd, char ch)
-{
-    int w = (h0 - jd) % N;
-    if (w < 0) w = w + N;
-    return ((w * M) % N + ch - 'a') % N;
-}
+    unsigned char ch;
+    int e, n;
 
-int Znajdz(string w, string t)
-{
-    int i, p = 0, dw = w.size(), dt = t.size();
-    int hw = Hash0(w);
-    int ht = Hash0(t.substr(0, dw));
-    int pot = Potega(M, dw - 1);
+    cout << "Podaj klucz publiczny:" << endl;
+    cout << "e = ";
+    cin >> e;
+    cout << "n = ";
+    cin >> n;
 
-    while (p <= dt - dw)
-    {
-        if (hw == ht)
-        {
-            i = 0;
-            while (i < dw && w[i] == t[p + i]) i++;
-            if (i == dw) return p;
-        }
+    we >> noskipws;
 
-        if (p < dt - dw)
-            ht = Hash1(ht, (pot * (t[p] - 'a')) % N, t[p + dw]);
+    while (we >> ch)
+        wy << RSA(ch, e, n) << " ";
 
-        p++;
-    }
+    we.close();
+    wy.close();
 
-    return -1;
+    cout << "Tekst zostal zaszyfrowany";
+
+    return 0;
 }
